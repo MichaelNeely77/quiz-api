175986f8bbe9a0f72c62b6a23b3cc22793683cb5
Michael Neely <michael@michaelneely.net>
2020-06-03 23:45:58 -0400

    Added the basic funcitons

--- js/quiz.js
+++ js/quiz.js
@@ -6,6 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // loads new question from API
 loadQuestion = () => {
-    console.log('from loadQuestion');
+    const url = 'https://opentdb.com/api.php?amount=1';
+    fetch(url)
+    .then(data => data.json())
+    .then(result => displayQuestion(result.results));
+}
+
+displayQuestion = question => {
+    console.log(question);
 }
 
