73ac2a879c6a68bc639d0cf04ddf67ae624239a0
Michael Neely <michael@michaelneely.net>
2020-06-03 23:58:11 -0400

    Added the question array

--- js/quiz.js
+++ js/quiz.js
@@ -12,7 +12,20 @@ loadQuestion = () => {
     .then(result => displayQuestion(result.results));
 }
 
-displayQuestion = question => {
-    console.log(question);
+displayQuestion = questions => {
+
+    let array1 = [1,2.,3];
+    let value1 = 4;
+
+    array1.splice( Math.floor(Math.random() * 3), 0, value1);
+
+    console.log(array1);
+
+    const questionHTML = document.createElement('div');
+    questionHTML.classList.add('col-12');
+
+    questions.forEach(question => {
+        console.log(question);
+    });
 }
 
