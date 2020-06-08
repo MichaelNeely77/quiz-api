c0c1d7daca75d210e732fd82baff6f598dbd6ef6
Michael Neely <michael@michaelneely.net>
2020-06-07 23:59:13 -0400

    Added the category, question, and answers to the DOM

--- js/quiz.js
+++ js/quiz.js
@@ -29,9 +29,31 @@ displayQuestion = questions => {
         let possibleAnswers = question.incorrect_answers;
         possibleAnswers.splice( Math.floor( Math.random() * 3 ), 0, correctAnswer );
 
-        console.log(possibleAnswers);
+        // Add HTML for current question
+        questionHTML.innerHTML = `
+            <div class="row justify-content-between heading">
+                <p class="category">Category: ${question.category}</p>
 
-        console.log(question);
+            </div>
+            <h2 class="text-center">${question.question}</h2>
+        `;
+
+        // GEnerate HTML for possible answers
+        const answerDiv = document.createElement('div');
+
+        answerDiv.classList.add('questions', 'row', 'justify-content-around', 'mt-4');
+        possibleAnswers.forEach(answer => {
+            const answerHTML = document.createElement('li');
+            answerHTML.classList.add('col-12', 'col-md-5');
+            answerHTML.textContent = answer;
+            answerDiv.appendChild(answerHTML);
+            
+        });
+
+        questionHTML.appendChild(answerDiv);
+
+        //Render question in the browser
+        document.querySelector('#app').appendChild(questionHTML);
     });
 }
 
